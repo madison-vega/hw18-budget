f4cc5e018ed134426cd866d02007ab0ed9a39bbf
Madison Vega <madison.r.vega@gmail.com>
2021-06-05 17:12:50 -0500

    added code for atlas db

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 // routes
 app.use(require("./routes/api.js"));
 
