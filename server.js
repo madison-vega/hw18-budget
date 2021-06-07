3c3c75abee6b64ab2057c1f4cdfc734e47b645c4
Madison Vega <madison.r.vega@gmail.com>
2021-06-07 18:56:35 -0500

    testing deployment issue

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget_db", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/budget_db',
